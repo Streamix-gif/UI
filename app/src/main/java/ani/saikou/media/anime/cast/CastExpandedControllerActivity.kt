2aa9bdb1c0c170165c5a09f7db978432fb699a71
Streamix-gif <enunglasmana@gmail.com>
2026-09-22 19:29:42 +0700

    player: add Dantotsu playback dependencies

---
+++ app/src/main/java/ani/saikou/media/anime/cast/CastExpandedControllerActivity.kt
@@ -0,0 +1,15 @@
+package ani.saikou.media.anime.cast
+
+import android.view.Menu
+import com.google.android.gms.cast.framework.CastButtonFactory
+import com.google.android.gms.cast.framework.media.widget.ExpandedControllerActivity
+import ani.saikou.R
+
+class CastExpandedControllerActivity : ExpandedControllerActivity() {
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.cast_menu, menu)
+        CastButtonFactory.setUpMediaRouteButton(applicationContext, menu, R.id.media_route_menu_item)
+        return true
+    }
+}
