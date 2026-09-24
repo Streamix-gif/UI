819ae5f7dd4fe983eeba4fc980c1f30f74204b32
Streamix-gif <enunglasmana@gmail.com>
2026-09-25 00:38:17 +0700

    fix: use safe anime page top inset

--- app/src/main/java/ani/dantotsu/home/AnimeFragment.kt
+++ app/src/main/java/ani/dantotsu/home/AnimeFragment.kt
@@ -11,7 +11,6 @@ import android.view.ViewGroup
 import android.view.animation.OvershootInterpolator
 import androidx.core.content.ContextCompat
 import androidx.core.view.marginBottom
-import androidx.core.view.updateLayoutParams
 import androidx.core.view.updatePaddingRelative
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -97,10 +96,10 @@ class AnimeFragment : Fragment() {
             Refresh.activity[this.hashCode()]!!.postValue(true)
         }
 
-        binding.animePageRecyclerView.updatePaddingRelative(bottom = navBarHeight + 160f.px)
-        binding.animePageRecyclerView.updateLayoutParams<ViewGroup.MarginLayoutParams> {
-            topMargin = statusBarHeight + 12f.px
-        }
+        binding.animePageRecyclerView.updatePaddingRelative(
+            top = statusBarHeight + 12f.px,
+            bottom = navBarHeight + 160f.px
+        )
 
         animePageAdapter = AnimePageAdapter()
 
